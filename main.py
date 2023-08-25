3d6eefb68a91ada67aa71b2d07a6adb959a8142d
unknown <afsj3@cesar.school>
2023-08-24 23:20:51 -0300

    commitando2

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.services.service_user import ServiceUser
 
 
 service = ServiceUser()
-resultado1 = service.add_user("Sergio", 0)
+resultado1 = service.add_user("Antonio", 0)
 
 print("")
 print(resultado1)
